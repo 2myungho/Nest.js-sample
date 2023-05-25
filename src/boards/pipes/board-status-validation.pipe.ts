68e40cf536e1cdd819f8480b8a2ada4461ccdede
2300020 <2300020@partner.skcc.com>
2023-05-25 13:43:32 +0900

    first commit

---
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -0,0 +1,21 @@
+import { BadRequestException, PipeTransform } from '@nestjs/common';
+import { BoardStatus } from '../board.model';
+
+export class BoardStatusValidationPipe implements PipeTransform {
+  readonly StatusOptions = [BoardStatus.PRIBATE, BoardStatus.PUBLIC];
+
+  transform(value: any) {
+    value = value.toUpperCase();
+
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`${value} isn't in the status option`);
+    }
+
+    return value;
+  }
+
+  private isStatusValid(status: any) {
+    const index = this.StatusOptions.indexOf(status);
+    return index !== -1;
+  }
+}
